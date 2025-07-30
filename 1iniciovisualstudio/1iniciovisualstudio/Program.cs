6aa5edea33f12d193e06353638af87c2525ab69e
Isabela Jimenez <isabela.jimenezl@upb.edu.co>
2025-07-30 08:46:37 -0500

    este es mi primer commit

---
+++ 1iniciovisualstudio/1iniciovisualstudio/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1iniciovisualstudio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
