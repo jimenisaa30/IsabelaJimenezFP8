c2c84bb5b82608d8dd3f52c6e5e399e08fcde1ee
Isabela Jiménez <158300011+jimenisaa30@users.noreply.github.com>
2025-08-01 09:54:23 -0500

    Add files via upload
    Variables, constantes, tipos de datos

--- 1iniciovisualstudio/1iniciovisualstudio/Program.cs
+++ 1iniciovisualstudio/1iniciovisualstudio/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Declaración e inicialización de variables
+            string nombre= "Isabela";
+            const string iva = "19%";
+            string apellidos = "Jiménez López";
+
+            Console.WriteLine(nombre + ", Bienvenido a Fundamentos de programación");
+            nombre = "Alejandro";// como la variable nombre no es constante l apuedo cambiar incluso después de inicializarlo
+            // iva = "20%";No puedo cambiar el valor de la variable iva por ser una constante
+            Console.Write("\t"+ nombre +" "+ apellidos +" "+ iva);
+            Console.ReadKey(); // Para que el usuario toque cualquier tecla y el programa se termine de ejecutar
         }
     }
 }
--- 1iniciovisualstudio/1iniciovisualstudio/obj/Debug/net8.0/1iniciovisualstudio.AssemblyInfo.cs
+++ 1iniciovisualstudio/1iniciovisualstudio/obj/Debug/net8.0/1iniciovisualstudio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1iniciovisualstudio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24d73b002792edbbcf45e4f70162c5897b918589")]
 [assembly: System.Reflection.AssemblyProductAttribute("1iniciovisualstudio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1iniciovisualstudio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
