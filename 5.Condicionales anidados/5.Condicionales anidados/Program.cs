6f33c1f8d7ea0756a9e65b87a15ccdee087a4e33
Isabela Jiménez <158300011+jimenisaa30@users.noreply.github.com>
2025-08-15 09:32:40 -0500

    Condicionales anidados

---
+++ 5.Condicionales anidados/5.Condicionales anidados/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _5.Condicionales_anidados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejercicio 1. 
+            
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+
+            Console.WriteLine("Ingrese el número 1:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el número 2:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el número 3:");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.WriteLine($"El número 1 {num1} es el mayor.");
+                }
+                else 
+                {
+                    Console.WriteLine($"El número 3 {num3} es el mayor.");
+
+                }
+            }
+            else 
+            {
+                if (num2 > num3)
+                {
+                    Console.WriteLine($"El número 2 {num2} es el mayor.");
+                }
+                else
+                {
+                    Console.WriteLine($"El número 3 {num3} es el mayor.");
+
+                }
+            }
+        }
+    }
+}
