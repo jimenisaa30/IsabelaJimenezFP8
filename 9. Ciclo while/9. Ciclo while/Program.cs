ac050f43b70c9ec3f844e139078bd4ae6adb598f
Isabela Jiménez <158300011+jimenisaa30@users.noreply.github.com>
2025-08-29 09:34:42 -0500

    Estructuras de control : while

---
+++ 9. Ciclo while/9. Ciclo while/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _9._Ciclo_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+            int acumulador = 0;
+            
+            while (contador <= 5)
+            {
+                acumulador += contador; // al cuamulador se le suma lo que lleva el contador
+                Console.WriteLine($"Contador = {contador} , Acumulador = {acumulador}"); // muestra el proceso
+                contador++; // el contador aumenta en 1
+            }
+
+            Console.WriteLine($"La suma de los primeros 5 números enteros es: {acumulador}");
+        }
+    }
+}
