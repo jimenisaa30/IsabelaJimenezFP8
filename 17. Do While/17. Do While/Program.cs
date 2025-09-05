54ebc719908c365349f799c33ed7c26bf236643e
Isabela Jiménez <158300011+jimenisaa30@users.noreply.github.com>
2025-09-05 10:22:40 -0500

    Ejercicios ciclo "do while"

---
+++ 17. Do While/17. Do While/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace _17._Do_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Algoritmo que solicita un número y genere su correspondiente tabla de multiplicar desde el 1 hasta el 10. Y así sucesivamente hasta que el usuario ya no desee continuar generando tablas de multiplicar. 
+
+            /*
+            int contador = 0; //Inicia cada número entero
+            int acumulador = 0; //Va guradando la suma de los números
+
+            do
+            {
+                contador++;
+                acumulador += contador;
+
+            } while (contador <5 );
+
+            Console.WriteLine($"La suma de los primeros 5 números enteros es de {acumulador}");
+            */
+
+            /*Algoritmo que solicita un número y genere su correspondiente tabla de multiplicar desde el 1 hasta el 10. Y así sucesivamente hasta que el usuario ya no desee continuar generando tablas de multiplicar. */
+
+            
+            int numero = 0;
+            string continuar = " ";
+
+            int contador = 1;
+
+            /*
+            FORMA 1 (yo)
+            Console.WriteLine("Ingrese un número del 1 al 10 para ver su tabla de multiplicación");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+             do
+            {
+                Console.WriteLine($"{numero} x 1 = {numero * 1}");
+                Console.WriteLine($"{numero} x 2 = {numero * 2}");
+                Console.WriteLine($"{numero} x 3 = {numero * 3}");
+                Console.WriteLine($"{numero} x 4 = {numero * 4}");
+                Console.WriteLine($"{numero} x 5 = {numero * 5}");
+                Console.WriteLine($"{numero} x 6 = {numero * 6}");
+                Console.WriteLine($"{numero} x 7 = {numero * 7}");
+                Console.WriteLine($"{numero} x 8 = {numero * 8}");
+                Console.WriteLine($"{numero} x 9 = {numero * 9}");
+                Console.WriteLine($"{numero} x 10 = {numero * 10}");
+
+                Console.WriteLine("Desea generar otra tabla de multiplicar? Sí: s No: n");
+                continuar = Convert.ToChar(Console.ReadLine());
+
+            } while (continuar == 's'); */
+
+            //FORMA 2 (profe)
+
+
+            do
+            {
+                Console.WriteLine("Ingrese un número del 1 al 10 para ver su tabla de multiplicación");
+                numero = Convert.ToInt32(Console.ReadLine());
+
+                do
+                {
+                    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+                    contador++;
+
+                } while (contador <= 10);
+                
+                contador = 1;//Reinicia el contador para evitar errores
+
+                Console.WriteLine("Desea generar otra tabla de multiplicar? Sí: s No: n");
+                continuar =Console.ReadLine().ToLower(); //Convierte todo a minúscula en caso de que el usuario ingrese mayúsculas
+
+            } while (continuar == "s"); //Variable bandera (bool, char, números)
+
+            //Podemos generar ciclos anidados pero estos "Complican" el algoritmo lo que hace que requiera más máquina.
+            
+
+
+        }
+    }
+}
---
+++ 18. Do While/18. Do While/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _18._Do_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Algoritmo que le pida a un grupo de usuarios su nombre, número de cuenta y saldo; para cada usuario se debe hacer lo siguiente: 
+                Si el saldo es mayor a 3’000.000, sacar un mensaje que diga el nombre del usuario, el número de cuenta, el saldo y “Es apto para el crédito” 
+                Si el saldo es menor a 3’000.000, sacar un mensaje que diga el nombre del usuario, el número de cuenta, el saldo y “No es apto para el crédito” 
+                El algoritmo debe permitir preguntar la información de usuarios, hasta que se le indique que ya no se desea preguntar más. 
+
+            Además, el algoritmo debe permitir mostrar el número de usuarios a los que se le pregunto por la información, y debe mostrar el promedio de los saldos. */
+
+            string nombre = " ";
+            int NumCuenta = 0;
+            float saldo = 0.0f;
+
+            string continuar = " ";
+
+            int NumUsuarios = 0; //Contador
+            float PromSaldos = 0.0f;
+
+            do
+            {
+                Console.WriteLine("Ingrese su nombre");
+                nombre = Console.ReadLine();
+
+                Console.WriteLine("Ingrese su número de cuenta");
+                NumCuenta = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Ingrese su saldo actual");
+                saldo = Convert.ToSingle(Console.ReadLine());
+
+                if (saldo >= 3000000f)
+                {
+                    Console.WriteLine($"El usuario {nombre} con número de cuenta {NumCuenta} y saldo {saldo} es apto para el crédito ");
+                }
+                else if (saldo <= 3000000f)
+                {
+                    Console.WriteLine($"El usuario {nombre} con número de cuenta {NumCuenta} y saldo {saldo} no es apto para el crédito ");
+                }
+
+                NumUsuarios++;
+                Console.WriteLine($"El número de usuarios encuestados es {NumUsuarios}");
+
+                PromSaldos += saldo / NumUsuarios;
+                Console.WriteLine(PromSaldos);
+
+                Console.WriteLine("Desea ingresar más información sobre los usuarios? Sí: s - No: n");
+                continuar = Console.ReadLine().ToLower();
+
+            } while (continuar == "s");
+        }
+    }
+}
