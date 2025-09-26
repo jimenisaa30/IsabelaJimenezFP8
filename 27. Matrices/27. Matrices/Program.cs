6034b0b7d23e540214347372e93598978dc40d4d
Isabela Jimenez Lopez <isabela.jimenezl@upb.edu.co>
2025-09-26 09:28:42 -0500

    Matrices y ejercicio

---
+++ 27. Matrices/27. Matrices/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace _27._Matrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaración de matrices
+            int[,] numeros = new int[2,2];
+            string[,] nombres = new string[2,3];
+            char[,] caracteres = new char[3,2];
+
+            //Ingresar datos
+
+            numeros[0, 0] = 20;
+            numeros[0, 1] = 35;
+            numeros[1, 0] = 42;
+            numeros[1, 1] = 78;
+
+            //Otra forma de declarar e inicalizar matrices
+            /* string[,] apellidos =
+             {
+                 {"Jiménez","Cardona" },
+                 {"Buitrago", "Arias"},
+                 {"Acevedo" , "Ortiz" }
+             };
+
+             //Recuperar datos de la matriz
+             Console.WriteLine(apellidos[1, 1]);
+
+             //Recorres matrices
+             for(int i = 0; i < 3; i++)//Recorre las filas
+             {
+                 for(int j = 0;j < 2; j++)//Recorre las columnas en cada una de la filas
+                 {
+                     Console.WriteLine($"Ingrese el caracter para la posición con índices{i}, {j}");
+                     caracteres[i,j] = char.Parse(Console.ReadLine());
+                 }
+             }
+
+             //Recorrer matrices para recuperar datos
+             for(int i = 0;i < caracteres.GetLength(0); i++) //.GetLenght con 0 devuelve el número de filas
+             {
+                 for (int j = 0; j < caracteres.GetLength(1); j++) //Con 1 devuelve el número de columnas
+                 {
+                     Console.WriteLine ($"Caracter posición {i},{j}: {caracteres[i,j]}");
+                 }
+                 Console.WriteLine();
+             }
+
+             //Rango de la matriz
+             Console.WriteLine(caracteres.Length);*/
+
+            //EJERCICIO
+            int[,] numDiez =
+            {
+                { 10,10,10,10 },
+                { 10,10,10,10 },
+                { 10,10,10,10 },
+                { 10,10,10,10 }
+            };
+
+            for (int i = 0; i < numDiez.GetLength(0); i++) //.GetLenght con 0 devuelve el número de filas
+            {
+                for (int j = 0; j < numDiez.GetLength(1); j++) //Con 1 devuelve el número de columnas
+                {
+                    Console.WriteLine($"Caracter posición {i},{j}: {numDiez[i, j]}");
+                }
+                Console.WriteLine();
+            }
+
+        } 
+    }
+}
---
+++ 28. Matrices/28. Matrices/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _28._Matrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escribe un programa que realice la suma de dos matrices de dimensiones 2x3.
+ 
+                Requisitos del programa:
+                Solicita al usuario que ingrese los elementos de la primera matriz de 2 filas y 3 columnas.
+                Solicita al usuario que ingrese los elementos de la segunda matriz de las mismas dimensiones (2x3).
+                Calcula la matriz suma, resultado de sumar cada elemento correspondiente de las dos matrices.
+                Muestra la matriz resultante de la suma en formato de matriz (2 filas, 3 columnas).*/
+
+            int[,] matriz1 = new int[2,3];
+            int[,] matriz2 = new int[2,3];
+            int[,] matrizSuma = new int[2,3];
+
+            for (int i = 0; i < 2; i++)//Recorre la matriz 1
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine($"Ingrese el número para la posición con índices{i}, {j} en la matriz #1");
+                    matriz1[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < 2; i++)//Recorre la matriz 2
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine($"Ingrese el número para la posición con índices{i}, {j} en la matriz #2");
+                    matriz2[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matrizSuma[i, j] = matriz1[i, j] + matriz2[i,j];
+                }
+            }
+
+            for (int i = 0; i < matrizSuma.GetLength(0); i++) //.GetLenght con 0 devuelve el número de filas
+            {
+                for (int j = 0; j < matrizSuma.GetLength(1); j++) //Con 1 devuelve el número de columnas
+                {
+                    Console.WriteLine(matrizSuma[i, j]);
+                }
+                Console.WriteLine();
+            }
+        } 
+    }
+}
