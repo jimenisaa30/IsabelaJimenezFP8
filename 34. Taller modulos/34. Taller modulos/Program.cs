d1e8d06a77ac0beaeb87159489f9dd41bb3a513d
Isabela Jiménez <158300011+jimenisaa30@users.noreply.github.com>
2025-10-15 09:35:00 -0500

    Taller Modulos

---
+++ 34. Taller modulos/34. Taller modulos/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Numerics;
+
+namespace _34._Taller_modulos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string PalabraIngresada=IngresarPalabra();
+            VectorCar(PalabraIngresada); //Este módulo debe contar con un parámetro en el contexto del Main
+            ContarLetras(PalabraIngresada);
+        }
+        static string IngresarPalabra()
+        {
+            Console.WriteLine("Ingrese una palabra");
+            string palabra = Console.ReadLine();
+
+            return palabra;
+        }
+
+        static char[] VectorCar(string palabra)
+        {
+            
+            for (int i = 0; i < palabra.Length; i++)
+            {
+                Console.Write(palabra[i] + "|");
+            }
+            Console.WriteLine();
+
+            return palabra.ToCharArray();
+        }
+
+        static void ContarLetras(char palabra)
+        {
+            do
+            {
+                int contadorVocales = 0;
+                int contadorConsonantes = 0;
+                if (palabra == 'a' && palabra == 'e' && palabra == 'i' && palabra == 'o' && palabra == 'u')
+                {
+                    contadorVocales++;
+                }
+                else
+                {
+                    contadorConsonantes++;
+                }
+            }while(true);
+
+            return contadorVocales;
+
+            Console.WriteLine($"La palabra ingresada contiene {contadorVocales}")
+        }
+
+    }
+}
